66f068724a400fc7568ecf2482fb69bb39114091
Chanuka vithanage <chanukavithanage5@gmail.com>
2025-05-19 15:16:34 +0530

    DBConnete correction

--- src/main/java/util/DBConnect.java
+++ src/main/java/util/DBConnect.java
@@ -12,7 +12,7 @@ public class DBConnect {
 		
 		Class.forName("com.mysql.jdbc.Driver");
 		
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/database?characterEncoding=utf8",username,password);
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gamudalk?characterEncoding=utf8",username,password);
 		
 		return con;
 	}
